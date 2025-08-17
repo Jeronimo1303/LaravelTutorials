89bdba8e57eacf3f2fa6c7b00967a74f9faac5b0
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-08-17 08:16:50 -0500

    Now displays the price values for each product

--- laravelcourse/app/Http/Controllers/ProductController.php
+++ laravelcourse/app/Http/Controllers/ProductController.php
@@ -5,23 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-
-    public static $products = [
-        ["id" => "1", "name" => "TV", "description" => "Best TV", "price" => 100],
-        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone", "price" => 20],
-        ["id" => "3", "name" => "Chromecast", "description" => "Best Chromecast", "price" => 200],
-        ["id" => "4", "name" => "Glasses", "description" => "Best Glasses", "price" => 50]
-    ];
 
     public function index(): View
     {
         $viewData = [];
         $viewData["title"] = "Products - Online Store";
         $viewData["subtitle"] = "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData["products"] = Product::all();
+
+
         return view('product.index')->with("viewData", $viewData);
 
     }
@@ -30,13 +26,18 @@ class ProductController extends Controller
     public function show(string $id)
     {
         $viewData = [];
-        if ($id > sizeof(ProductController::$products)) {
+        $product = Product::findOrFail($id);
+
+        /*if ($id > sizeof($produc)) {
             return redirect()->route('home.index');
-        }
-        $product = ProductController::$products[$id - 1];
+        }*/
+
+        [$id - 1];
         $viewData["title"] = $product["name"] . " - Product information";
         $viewData["subtitle"] = $product["name"] . " - Product information";
         $viewData["product"] = $product;
+
+
         return view('product.show')->with("viewData", $viewData);
     }
 
@@ -44,6 +45,8 @@ class ProductController extends Controller
     {
         $viewData = []; //to be sent to the view
         $viewData['title'] = "Create Product";
+
+
         return view('product.create')->with("viewData", $viewData);
     }
 
