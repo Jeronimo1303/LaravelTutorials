0e56cb0aa914681c7928c78f02b8edd54be6803e
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-08-22 07:21:58 -0500

    Added Image services using dependency inversion

---
+++ laravelcourse/app/Http/Controllers/ImageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Interfaces\ImageStorage;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ImageController extends Controller
+{
+
+    public function index()
+    {
+        return view('image.index');
+    }
+
+    public function save(Request $request)
+    {
+        $storeInterface = app(ImageStorage::class);
+        $storeInterface->store($request);
+
+        return back();
+    }
+
+}
